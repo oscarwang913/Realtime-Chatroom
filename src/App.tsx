a64dbbe975b97debb6b7310c5eab16ef0331fdae
DESKTOP-JM65FNB\oscar <garnett284@gmail.com>
2022-06-20 23:41:52 +0800

    simple chatting function OK

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {io} from "socket.io-client"
 
 function App() {
+  const [value, setValue] = useState("")
+  const [chatMsg, setChatMsg] = useState<string[]>([])
+  const [socket, setSocket] = useState<any>(null)
+  
+  useEffect(()=> {
+    const socket = io("http://localhost:8080")
+    setSocket(socket)
+  }, [])
+
+  const handleSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    socket && socket.emit("chatting-message", value)
+    setValue("")
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value)
+  }
+
+  socket && socket.on("chatting-message", (msg:string) => {
+    setChatMsg([
+      ...chatMsg,
+      msg
+    ])
+  })
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {chatMsg && chatMsg.map((msg, index) => (
+        <h1 key={index}>{msg}</h1>
+      ))}
+      <form action="" onSubmit={handleSubmit}>
+        <input type="text" value={value} onChange={handleChange}/>
+        <button>Send</button>
+      </form>
     </div>
   );
 }
