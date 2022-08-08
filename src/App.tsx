72ff76b9a02a15914ced0e66677cf80fa46e3792
DESKTOP-JM65FNB\oscar <garnett284@gmail.com>
2022-08-09 06:42:50 +0800

    finish get online user and send message

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,85 @@
-import { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import { useState, useEffect, useRef } from 'react';
 import './App.css';
 import {io} from "socket.io-client"
 import { uniqueNamesGenerator, Config, adjectives, colors, animals } from 'unique-names-generator';
 
 
 interface ChatMessage {
-  user: string,
+  senderId: string,
   message: string
 }
 
+interface Users {
+  userId: string,
+  socketId: string
+}
+
 const customConfig: Config = {
-  dictionaries: [adjectives, colors],
+  dictionaries: [adjectives, animals],
   separator: '-',
   length: 2,
 };
 
 const shortName: string = uniqueNamesGenerator(customConfig);
-
+console.log(shortName)
 
 function App() {
   const [value, setValue] = useState("")
-  const [chatMsg, setChatMsg] = useState<ChatMessage[]>([])
-  const [socket, setSocket] = useState<any>(null)
-  useEffect(()=> {
-    const socket = io("http://localhost:8080")
-    setSocket(socket)
+  const [chatMsg, setChatMsg] = useState<any>([])
+  const [users, setUsers] = useState<Users[]>([])
+  const socket = useRef<any>()
+  const [arrivalMessage, setArrivalMessage] = useState<any>([])
+  
+  useEffect(() => {
+    console.log(1)
+    socket.current = io("http://localhost:8080")
+
+    socket.current.on('getMessage', (data: ChatMessage) => {
+      console.log(data)
+      setChatMsg([{
+        senderId: data.senderId,
+        message: data.message
+      }])
+    })
   }, [])
 
-  const handleSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    socket && socket.emit("chatting-message", {user: shortName, message: value})
-    setValue("")
-  }
+  useEffect(() => {
+    setArrivalMessage([...arrivalMessage, ...chatMsg])
+  }, [chatMsg])
 
+  useEffect(() => {
+    console.log(2)
+    socket.current.emit('addUser', shortName)
+    socket.current.on('getUsers', (onlineUsers: Users) => {
+      const onlineU = users.concat(onlineUsers)
+      setUsers(onlineU)
+    })
+  }, [shortName])
+  
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value)
   }
 
-  socket && socket.on("chatting-message", (obj:ChatMessage) => {
-    setChatMsg([
-      ...chatMsg, 
-      obj
-    ])
-  })
-  
+  const getReceiver = () => {
+    const receiver = users?.find(user => user.userId !== shortName)
+    return receiver?.userId
+  }
+
+  const handleSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const receiverId = getReceiver()
+    socket.current.emit('sendMessage', {
+      senderId: shortName,
+      receiverId,
+      message: value
+    })
+ }
+ console.log(chatMsg)
+ console.log(arrivalMessage)
   return (
     <div className="App">
-      {chatMsg && chatMsg.map((obj, index) => (
-        <h1 key={index} className={obj.user === shortName ? "you" : "other"}>{obj.user} {obj.message}</h1>
-      ))}
+      {users.length > 0 && users.map((user:Users, index: any) => (<h1 className="name" key={index}>{user.userId}</h1>))}
+      {arrivalMessage.length > 0 && arrivalMessage.map((message:ChatMessage, index:number) => (<h3 key={index}>{message.message}</h3>))}
       <form action="" onSubmit={handleSubmit}>
         <input type="text" value={value} onChange={handleChange}/>
         <button>Send</button>
